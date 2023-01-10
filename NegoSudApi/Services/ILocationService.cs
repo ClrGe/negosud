8fc930b955420dd24b2ac222a7ecbc19eaa32890
Slupshi <slupshi@gmail.com>
2023-01-10 16:04:48 +0100

    feat: location service

---
+++ NegoSudApi/Services/ILocationService.cs
@@ -0,0 +1,15 @@
+﻿using NegoSudApi.Models;
+
+namespace NegoSudApi.Services
+{
+    public interface ILocationService
+    {
+        public Task<Location?> GetLocation(int locationId);
+        public Task<IEnumerable<Location>?> GetLocations();
+        public Task<Location?> PostLocation(Location model);
+        public Task<Location?> PutLocation(Location model);
+        public Task DeleteLocation(int locationId);
+        public Task<IEnumerable<Bottle>?> GetBottles(int locationId);
+        public Task<IEnumerable<Storage>?> GetStorages(int locationId);
+    }
+}
