ba7d4771d75452931a63b971ac56a282a8ea3b34
Maxime Dandel <maxime.dandel@suez.com>
2023-01-26 19:17:12 +0100

    add migration to the versioning

--- NegoSudApi/Models/Role.cs
+++ NegoSudApi/Models/Role.cs
@@ -5,8 +5,8 @@ namespace NegoSudApi.Models;
 public class Role : IModelBase
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public int PermissionId { get; set; }
+    public string? Name { get; set; }
+    public int? PermissionId { get; set; }
     public DateTime? CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
     public string? CreatedBy { get; set; }
