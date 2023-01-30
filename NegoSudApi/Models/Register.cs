a2656da06e9c5c130812278f10c60aea5319457f
Maxime Dandel <maxime.dandel@suez.com>
2023-01-30 12:07:32 +0100

    change the register parameter

---
+++ NegoSudApi/Models/Register.cs
@@ -0,0 +1,8 @@
+namespace NegoSudApi.Models;
+
+public class Register
+{
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public string? Username { get; set; }
+}
