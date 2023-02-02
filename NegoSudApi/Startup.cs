a78aa52c0a4818d049d9062adda6ed0bc7726e1a
Clg <cg@jmg-conseil.eu>
2023-02-02 12:08:40 +0100

    add cors policy

--- NegoSudApi/Startup.cs
+++ NegoSudApi/Startup.cs
@@ -1,7 +1,12 @@
 ﻿using System.Text;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Cors;
+using Microsoft.CodeAnalysis.Options;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using NegoSudApi.Services;
 using NegoSudApi.Services.Interfaces;
@@ -21,16 +26,22 @@ public class Startup
 
     public void ConfigureServices(IServiceCollection services)
     {
+        
+        // Add Cors
+        services.AddCors();
+        
+        
         services.AddMvc(options => options.EnableEndpointRouting = false)
             .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
         
+
         services.AddSession();
         services.AddHttpLogging((options) =>
         {
             options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.Request;
         });
-        
-        services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo {Title = "NegoSudWebAPI", Version = "v1"}));
+
+        services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "NegoSudWebAPI", Version = "v1" }));
 
         services.AddScoped<IGrapeService, GrapeService>();
         services.AddScoped<IBottleService, BottleService>();
@@ -50,7 +61,7 @@ public class Startup
         services.AddScoped<SecurePassword>();
         services.AddTransient<IJwtAuthenticationService, JwtAuthenticationService>();
 
-        
+
         services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -76,12 +87,14 @@ public class Startup
 
         var connectionString = Configuration.GetConnectionString("DefaultNegoSudDbContext") ??
                                throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-        
+
         services.AddDbContext<NegoSudDbContext>(options =>
             options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention());
+        
+
     }
 
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, NegoSudDbContext dbContext)
+public void Configure(IApplicationBuilder app, IWebHostEnvironment env, NegoSudDbContext dbContext)
     {
         dbContext.Database.Migrate();
         app.UseSwagger();
@@ -90,9 +103,14 @@ public class Startup
             c.SwaggerEndpoint("/swagger/v1/swagger.json", "NegoSudWebAPI");
             c.RoutePrefix = "api/doc";
         });
-
+        // global cors policy
+        app.UseCors(x => x
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .SetIsOriginAllowed(origin => true) // allow any origin
+            .AllowCredentials()); // allow credentials    
         app.UseHttpLogging();
-        app.UseHttpsRedirection();
+        //app.UseHttpsRedirection();
         app.UseSession();
         app.UseRouting();
         app.UseAuthentication();
