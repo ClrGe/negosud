8ce8b93fe2458b6a93347b95b78e96e303248b45
Slupshi <slupshi@gmail.com>
2023-02-03 16:26:07 +0100

    feat: token in cookie in login response

--- NegoSudApi/Controllers/AuthenticationController.cs
+++ NegoSudApi/Controllers/AuthenticationController.cs
@@ -1,5 +1,8 @@
+using System.Net;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using NegoSudApi.DTO;
 using NegoSudApi.Models;
 using NegoSudApi.Services;
 using NegoSudApi.Services.Interfaces;
@@ -25,24 +28,43 @@ public class AuthenticationController : ControllerBase
     
     [HttpPost]
     [Route("Login")]
-    public Task<ActionResult<string>> Login(Register register)
+    public Task<ActionResult<AuthResponse>> Login(Register register)
     {
-        var dbUser = _jwtAuthenticationService.Authenticate(register.Email, register.Password);
+        User? dbUser = _jwtAuthenticationService.Authenticate(register.Email, register.Password);
         if (dbUser != null)
         {
-            var claims = new List<Claim>
+            List<Claim> claims = new List<Claim>
             {
                 new(ClaimTypes.Email, register.Email)
             };
-            var token = _jwtAuthenticationService.GenerateToken(_configuration["Jwt:Key"]!, claims);
-            return Task.FromResult<ActionResult<string>>(Ok(token));
+            string? token = _jwtAuthenticationService.GenerateToken(_configuration["Jwt:Key"]!, claims);
+            AuthResponse response = new AuthResponse()
+            {
+                Id = dbUser.Id,
+                FirstName = dbUser.FirstName,
+                LastName = dbUser.LastName,
+                Email = dbUser.Email
+            };
+
+            Response.Cookies.Append(
+            "session",
+            token,
+            new CookieOptions()
+            {
+                IsEssential = true,
+                HttpOnly = false,
+                Secure = false,
+                SameSite = SameSiteMode.Lax,
+            }
+            );
+           return Task.FromResult<ActionResult<AuthResponse>>(Ok(response));
         }
-        return Task.FromResult<ActionResult<string>>(Unauthorized());
+        return Task.FromResult<ActionResult<AuthResponse>>(Unauthorized());
     }
-    
+
     [HttpPost]
     [Route("Register")]
-    public async Task<ActionResult<string>> Register(Register register)
+    public async Task<ActionResult<AuthResponse>> Register(Register register)
     {
         var userToAdd = new User
         {
@@ -55,6 +77,33 @@ public class AuthenticationController : ControllerBase
 
         if (dbUser == null) return StatusCode(StatusCodes.Status204NoContent, $"No match - could not add content.");
 
-        return StatusCode(StatusCodes.Status201Created, dbUser.Email);
+        List<Claim> claims = new List<Claim>
+        {
+            new(ClaimTypes.Email, dbUser.Email)
+        };
+
+        string? token = _jwtAuthenticationService.GenerateToken(_configuration["Jwt:Key"]!, claims);
+
+        AuthResponse response = new AuthResponse()
+        {
+            Id = dbUser.Id,
+            FirstName = dbUser.FirstName,
+            LastName = dbUser.LastName,
+            Email = dbUser.Email
+        };
+
+        Response.Cookies.Append(
+            "session",
+            token,
+            new CookieOptions()
+            {
+                IsEssential = true,
+                HttpOnly = false,
+                Secure = false,
+                SameSite = SameSiteMode.Lax,
+            }
+            );
+
+        return StatusCode(StatusCodes.Status201Created, response);
     }
 }
