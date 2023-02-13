2abb7b4b9fa3219318b27bcf3ab1b20431dc6313
Maxime Dandel <maxime.dandel@suez.com>
2023-02-13 18:11:44 +0100

    adjustment

--- NegoSudApi/DTO/Register.cs
+++
@@ -1,8 +0,0 @@
-namespace NegoSudApi.Models;
-
-public class Register
-{
-    public string? Email { get; set; }
-    public string? Password { get; set; }
-    public string? Username { get; set; }
-}
