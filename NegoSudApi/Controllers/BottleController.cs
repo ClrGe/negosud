91e8ef590727f180bc0d72bbe0d5b9eafc5c64e6
ClG <94359348+ClrGe@users.noreply.github.com>
2023-02-26 15:59:52 +0100

    mass delete bottles (#68)

--- NegoSudApi/Controllers/BottleController.cs
+++ NegoSudApi/Controllers/BottleController.cs
@@ -52,6 +52,7 @@ public class BottleController : ControllerBase
     return StatusCode(StatusCodes.Status201Created, dbBottle);
     }
 
+    [Authorize(Policy = RolePermissions.CanAddBottle)]
     [HttpPost("MassAddBottle")]
     public async Task<IActionResult> MassAddBottleAsync(ICollection<Bottle> bottles)
     {
@@ -77,15 +78,14 @@ public class BottleController : ControllerBase
     return StatusCode(StatusCodes.Status200OK, dbBottle);
     }
 
+    [Authorize(Policy = RolePermissions.CanEditBottle)]
     [HttpPost("MassUpdateBottle")]
     public async Task<IActionResult> MassUpdateBottleAsync(ICollection<Bottle> bottles)
     {
         if (bottles == null) return BadRequest();
 
         ICollection<Bottle>? dbBottles = await _bottleService.MassUpdateBottleAsync(bottles);
-
-               return dbBottles == null ? StatusCode(StatusCodes.Status404NotFound, "No match, no changes were made.") : StatusCode(StatusCodes.Status200OK, dbBottles);
-
+        return dbBottles == null ? StatusCode(StatusCodes.Status404NotFound, "No match, no changes were made.") : StatusCode(StatusCodes.Status200OK, dbBottles);
     }
 
     [Authorize(Policy = RolePermissions.CanDeleteBottle)]
@@ -98,5 +98,14 @@ public class BottleController : ControllerBase
         return StatusCode(StatusCodes.Status404NotFound, $"No bottle found for id: {id} - could not be deleted");
 
     return StatusCode(StatusCodes.Status200OK);
+    }
+    
+    [Authorize(Policy = RolePermissions.CanDeleteBottle)]
+    [HttpPost("MassDeleteBottle")]
+    public async Task<IActionResult> MassDeleteBottleAsync(ICollection<Bottle> bottles)
+    {
+        ICollection<Bottle>? dbBottles = await _bottleService.MassDeleteBottleAsync(bottles);
+
+        return bottles == null ? StatusCode(StatusCodes.Status404NotFound, "No match, no deletion.") : StatusCode(StatusCodes.Status200OK, dbBottles);
     }
 }
